876ae08a0a9ccdd1a1da822a3677d1147e9e9f14
Jacques <34221349+jchi5f@users.noreply.github.com>
2021-02-02 15:52:55 +0100

    NEW(CardType) Mastercard BINS

--- CreditCardEntry/src/com/devmarvel/creditcardentry/library/CardType.java
+++ CreditCardEntry/src/com/devmarvel/creditcardentry/library/CardType.java
@@ -9,14 +9,14 @@ import java.io.Serializable;
 class CardRegex {
     // See: http://www.regular-expressions.info/creditcard.html
     static final String REGX_VISA = "^4[0-9]{15}?"; // VISA 16
-    static final String REGX_MC = "^5[1-5][0-9]{14}$"; // MC 16
+    static final String REGX_MC = "^5[1-9][0-9]{14}$"; // MC 16
     static final String REGX_AMEX = "^3[47][0-9]{13}$"; // AMEX 15
     static final String REGX_DISCOVER = "^6(?:011|5[0-9]{2})[0-9]{12}$"; // Discover 16
     static final String REGX_DINERS_CLUB = "^3(?:0[0-5]|[68][0-9])[0-9]{11}$"; // DinersClub 14
     static final String REGX_JCB = "^35[0-9]{14}$"; // JCB 16
 
     static final String REGX_VISA_TYPE = "^4[0-9]{3}?"; // VISA 16
-    static final String REGX_MC_TYPE = "^5[1-5][0-9]{2}$"; // MC 16
+    static final String REGX_MC_TYPE = "^5[1-9][0-9]{2}$"; // MC 16
     static final String REGX_AMEX_TYPE = "^3[47][0-9]{2}$"; // AMEX 15
     static final String REGX_DISCOVER_TYPE = "^6(?:011|5[0-9]{2})$"; // Discover 16
     static final String REGX_DINERS_CLUB_TYPE = "^3(?:0[0-5]|[68][0-9])[0-9]$"; // DinersClub 14
